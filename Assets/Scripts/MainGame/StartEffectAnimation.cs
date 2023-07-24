9bdc94b2efdabf84d395d59e4540ed371a4b23a1
sabanisi <taisuke140909@gmail.com>
2023-07-24 15:47:17 +0900

    スタート演出リファクタ

--- Assets/Scripts/MainGame/StartEffectAnimation.cs
+++ Assets/Scripts/MainGame/StartEffectAnimation.cs
@@ -1,42 +1,45 @@
+using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
 
 namespace Sabanishi.MainGame
 {
-    public class StartEffectAnimation:MonoBehaviour
+    public class StartEffectAnimation : MonoBehaviour
     {
         [SerializeField] private Sprite _image1;
         [SerializeField] private Sprite _image2;
         [SerializeField] private Sprite _image3;
         [SerializeField] private Sprite _image4;
-        
+
         public void StartEffect(Vector3 startPos)
         {
             for (var i = 0; i < 5; i++)
             {
-                CreateEffect(startPos,i);
+                CreateEffect(startPos, i);
             }
         }
-        
-        private void CreateEffect(Vector3 startPos,int i)
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        private void CreateEffect(Vector3 startPos, int i)
         {
             Sprite sprite = GetSprite(i);
             var obj = new GameObject($"StartEffect({i})");
-            obj.transform.parent= transform;
-            obj.transform.localScale = 2.5f*Vector3.one;
+            obj.transform.parent = transform;
+            obj.transform.localScale = 2.5f * Vector3.one;
             var sprr = obj.AddComponent<SpriteRenderer>();
             sprr.sprite = sprite;
             obj.transform.position = startPos;
 
-            obj.transform.DOMove(startPos + GetMovePos(i), 0.5f)
-                .SetEase(Ease.OutCubic)
-                .OnComplete(() =>
-                {
-                    sprr.DOFade(0, 0.3f).SetEase(Ease.Linear).OnComplete(()=>
-                    {
-                        Destroy(obj);
-                    });
-                });
+            async UniTask Animation()
+            {
+                await obj.transform.DOMove(startPos + GetMovePos(i), 0.5f).SetEase(Ease.OutCubic)
+                    .ToUniTask(cancellationToken: obj.GetCancellationTokenOnDestroy());
+                await sprr.DOFade(0, 0.3f).SetEase(Ease.Linear)
+                    .ToUniTask(cancellationToken: obj.GetCancellationTokenOnDestroy());
+                Destroy(obj);
+            }
+
+            Animation().Forget();
         }
 
         private Vector3 GetMovePos(int i)
@@ -44,11 +47,21 @@ namespace Sabanishi.MainGame
             float angle = 0;
             switch (i)
             {
-                case 0: angle = 170f; break;
-                case 1: angle = 130f; break;
-                case 2: angle = 90f; break;
-                case 3: angle = 50f; break;
-                case 4: angle = 10f; break;
+                case 0:
+                    angle = 170f;
+                    break;
+                case 1:
+                    angle = 130f;
+                    break;
+                case 2:
+                    angle = 90f;
+                    break;
+                case 3:
+                    angle = 50f;
+                    break;
+                case 4:
+                    angle = 10f;
+                    break;
                 default: break;
             }
 
